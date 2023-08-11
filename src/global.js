d85e6d5eee101ef285644c5c00106448034133d9
Anju Elza Achenkunju <108949835+AnjuElza@users.noreply.github.com>
2023-08-11 17:22:55 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 // export const API = "https://64c210affa35860baea12028.mockapi.io"
-// export const API = "https://movie-app-q6bh.onrender.com"
-export const API = "http://localhost:4000"
+ export const API = "https://movie-app-q6bh.onrender.com"
+//export const API = "http://localhost:4000"
